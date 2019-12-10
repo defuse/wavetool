a92b93c3862f2f2c1d8e0c51988c175f1f8bd13d
Taylor Hornby <taylor@defuse.ca>
2019-12-10 13:28:58 -0700

    MVP CLI arguments

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # waveutil
 
-`waveutil` is a processing utility for Serum wavetables. The options are:
+`waveutil` is a processing/analysis tool for Serum wavetables. It can:
+
+- Generate spectrograms
+- Filter harmonics in various ways:
+    - Keep only the even/odd harmonics
+    - Specify a bitmap of harmonics to keep
+    - Specify a repeating pattern of harmonics to keep
+    - Keep/remove branches of the wavetable's prime factorization tree.
+- Factor a wavetable into its prime factorization tree.
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+
+   let matches = clap_app!(waveutil =>
+        (version: "0.0.1")
+        (author: "Taylor Hornby <taylor@defuse.ca>")
+        (about: "Serum wavetable editing / analysis tool.")
+        (@subcommand spectrogram =>
+            (about: "Generates a spectrogram from a wavetable")
+            (@arg INPUT: +required "The wavetable to analyze")
+        )
+        (@subcommand factor =>
+            (about: "Factors a wavetable into its prime-multiple-of-fundamental components")
+            (@arg INPUT: +required "The wavetable to factor")
+            (@arg normalize: -n --normalize "Normalize the outputs")
+            (@arg shift: -s --shift "Shift harmonics down to the fundamental")
+            (@arg recursive: -r --recursive "Factor generated wavetables recursively")
+        )
+        (@subcommand filter =>
+            (about: "Filter harmonics in various ways.")
+            (@arg INPUT: +required "The wavetable to be filtered")
+            (@arg OUTPUT: +required "Output file")
+            (@arg even: -e --even "Keep only the even harmonics")
+            (@arg odd: -o --odd "Keep only the odd harmonics")
+            (@arg BITMAP: -b --bitmap +takes_value "Keep only the harmonics specified by a bitmap.")
+            (@arg PATTERN: -p --pattern +takes_value "Repeat a bitmap up the spectrum")
+            (@arg KEEP_PRIMES: -k --keepprimes +takes_value "Keep only a set of prime-factorization tree branches")
+            (@arg REMOVE_PRIMES: -r --removeprimes +takes_value "Remove a set of prime-factorization tree branches")
+            (@arg protect: -f --fundamental "Protect the fundamental (overrides other filters)")
+            (@arg normalize: -n --normalize "Normalize the output")
+        )
+    )
+    .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("factor") {
+        println!("Factoring!");
+    }
 }
